f309080ea9580e3d169f541b700bd73dee2ebd42
Ajsa <besirovicajsa@gmail.com>
2025-08-15 15:22:50 +0200

    setTimeout, setInterval

--- javascript/34.cas/app.js
+++ javascript/34.cas/app.js
@@ -1,57 +1,104 @@
-// // const id = setInterval(() => {
-// //   console.log("Id timeout");
-// // }, 1000);
-// // //
-// // clearTimeout(id);
+// =====================
+// SINHRONI & ASINHRONI KOD – OBJAŠNJENO
+// =====================
 
-// function podeli(a, b) {
-//   if (b === 0) {
-//     throw new Error("Deljenje sa nulom nije dozvoljeno!");
-//   }
-//   return a / b;
-// }
+console.log("1");
+//   SINHRONO
+// Sinhroni kod se izvršava LINE BY LINE (liniju po liniju).
+// JavaScript ne prelazi na sledeću liniju dok se prethodna potpuno ne završi.
+// Ovo je tzv. "blocking" ponašanje – blokira dok se ne završi trenutna instrukcija.
 
-// console.log(podeli(10, 2)); // 5
-// try {
-//   console.log(podeli(10, 0)); //  Ovde bacamo grešku
-// } catch (e) {
-//   console.log(e.message);
-// }
-// console.log("ajsa");
+// ---------------------
+// setTimeout - macrotask primer
+// ---------------------
+setTimeout(() => {
+  console.log("2 - setTimeout (macrotask)");
+}, 0);
+//   ASINHRONO – MACROTASK
+// setTimeout(callback, vreme) zakazuje funkciju da se izvrši nakon
+// minimalno "vreme" milisekundi (ovde 0ms).
+// Iako je 0ms, callback NE ide odmah, već se stavlja u "macrotask queue"
+// i čeka da se:
+///   1. završi sav sinhroni kod
+///   2. završe svi microtaskovi
+// Tek onda Event Loop uzima ovaj zadatak i izvršava ga.
 
-// const prom = new Promise((resolve, reject) => {
-//   resolve("a");
-// });
-// console.log(prom);
-// prom.then((el) => console.log(el)).then((el) => console.log(el));
+// ---------------------
+// Promise - microtask primer
+// ---------------------
+const prom = new Promise((resolve, reject) => {
+  const condition = true;
 
-function makeRequest(location) {
-  return new Promise((resolve, reject) => {
-    console.log(`Making Request to ${location}`);
-    if (location === "Google") {
-      resolve("Google says hi");
-    } else {
-      reject("We can only talk to Google");
-    }
+  // Ovaj deo (unutar konstruktora Promisa) je SINHRONI kod:
+  if (condition) {
+    resolve("Bravo - Promise resolved");
+  } else {
+    reject("Ne brv - Promise rejected");
+  }
+});
+//   Napomena: Konstruktor Promisa se izvršava odmah,
+// ali .then() i .catch() callbackovi se izvršavaju ASINHRONO u "microtask queue".
+
+prom
+  .then((res) => {
+    console.log(res, "(microtask)");
+  })
+  .catch((e) => {
+    console.log(e, "(microtask)");
   });
-}
+// MICROTASK
+// Microtask queue ima VIŠI prioritet od macrotask queue.
+// To znači da će se .then() / .catch() izvršiti PRE bilo kog setTimeout-a,
+// bez obzira na to da li je setTimeout zakazan za 0ms.
 
-function processRequest(response) {
-  return new Promise((resolve, reject) => {
-    console.log("Processing response");
-    resolve(`Extra Information + ${response}`);
-  });
-}
+console.log("3");
+// SINHRONO – ispisuje se odmah, pre svih asinhronih callbackova.
 
-// makeRequest("Facebook")
-//   .then((res) => {
-//     return processRequest(res);
-//   })
-//   .then((res) => console.log(res))
-//   .catch((e) => console.log(e));
-// console.log("ajsa");
+// ---------------------
+// Demonstracija setTimeout i clearTimeout
+// ---------------------
+const idTimeout = setTimeout(() => {
+  console.log("Ovo se neće ispisati jer će biti obrisano");
+}, 1000);
+// MACROTASK – zakazan da se izvrši posle ~1000ms (1 sekunde)
 
-async function doWork() {
-  const res = await makeRequest("Google");
-}
-doWork();
+// clearTimeout(id) – uklanja zakazani timeout pre nego što se izvrši.
+setTimeout(() => {
+  clearTimeout(idTimeout); // briše gore zakazani timeout
+  console.log("Timeout obrisan pre izvršavanja (macrotask)");
+}, 500);
+// MACROTASK – posle 500ms briše prethodni timeout pre nego što on stigne na red.
+
+// ---------------------
+// setInterval primer
+// ---------------------
+let counter = 0;
+const id = setInterval(() => {
+  console.log("Interval:", counter);
+  counter++;
+
+  // Kada brojač dođe do 5, brišemo interval
+  if (counter == 5) {
+    clearInterval(id); // prekida zakazivanje
+  }
+}, 1000);
+// setInterval(callback, vreme) – izvršava callback svake "vreme" milisekundi.
+// Svaki poziv callbacka ide u macrotask queue.
+// Ako main thread nije slobodan, interval može kasniti (nije 100% precizan).
+
+// clearInterval(id) – prekida dalja ponavljanja.
+
+// ---------------------
+// BLOKIRANJE MAIN THREAD-a
+// ---------------------
+console.log("Blokiranje počinje");
+//  SINHRONO BLOKIRANJE – dok se ovaj while ne završi,
+// JavaScript neće preći ni na jedan asinhroni zadatak.
+// To znači da svi setTimeout/setInterval callbackovi i Promise .then() čekaju.
+
+let i = 0;
+while (i < 1000000000) i++; // simulacija teškog izračunavanja
+
+console.log("Blokiranje završeno");
+//   Tek sada Event Loop može da obradi microtaskove (Promise) i macrotaskove (setTimeout, setInterval)
+// koji su čekali dok je glavni thread bio zauzet.
